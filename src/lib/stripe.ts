44bd73429c33894d24542d0fd86c3098a66e972b
egil10 <egilfure@gmail.com>
2025-11-14 03:49:17 +0100

    f

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe'
 // Only initialize Stripe if the secret key is available
 export const stripe = process.env.STRIPE_SECRET_KEY
   ? new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: '2024-11-20.acacia',
+      apiVersion: '2023-10-16',
     })
   : null
 
